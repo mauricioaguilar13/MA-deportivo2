f011dea447458f6f6e9e9c88e48d3ebf2343d8d9
Mauricio aguilar <mauricio.aguilar@ujcv.edu.hn>
2026-03-19 03:00:41 -0600

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,72 +1,43 @@
 import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Tabs } from "expo-router";
-import { Stack } from "expo-router";
-
-export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "#007AFF",
-        tabBarInactiveTintColor: "gray",
-        headerShown: true,
-        headerTitle: "MA Deportivo",
-        headerTitleAlign: "center",
-        headerStyle: {
-          backgroundColor: "#007AFF",
-        },
-        headerTintColor: "#fff",
-        tabBarStyle: {
-          height: 60,
-          paddingBottom: 5,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="padel"
-        options={{
-          title: "Padel",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="star" size={24} color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="tennis"
-        options={{
-          title: "Tennis",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="circle-thin" size={24} color={color} />
-          ),
-        }}
-      />
-
+    <Tabs screenOptions={{ 
+      tabBarActiveTintColor: "#007AFF",
+      headerShown: true,
+      headerTitle: "MA Deportivo" 
+    }}>
       <Tabs.Screen
         name="futbol"
         options={{
           title: "Fútbol",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="soccer-ball-o" size={24} color={color} />
-          ),
+          tabBarIcon: ({ color }) => <FontAwesome size={24} name="soccer-ball-o" color={color} />,
         }}
       />
-
+      <Tabs.Screen
+        name="padel"
+        options={{
+          title: "Padel",
+          tabBarIcon: ({ color }) => <FontAwesome size={24} name="star" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="tennis"
+        options={{
+          title: "Tennis",
+          tabBarIcon: ({ color }) => <FontAwesome size={24} name="circle-thin" color={color} />,
+        }}
+      />
       <Tabs.Screen
         name="basket"
         options={{
           title: "Basket",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="dribbble" size={24} color={color} />
-          ),
+          tabBarIcon: ({ color }) => <FontAwesome size={24} name="dribbble" color={color} />,
         }}
       />
-
-      {/* OCULTAS (no salen en el menú) */}
+      {/* Ocultamos las que no estamos usando para evitar errores */}
       <Tabs.Screen name="index" options={{ href: null }} />
       <Tabs.Screen name="f1" options={{ href: null }} />
       <Tabs.Screen name="noticias" options={{ href: null }} />
