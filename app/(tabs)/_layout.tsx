1c6e5277e78efc29ef9c3d65b8235cb934d57bc3
Mauricio aguilar <mauricio.aguilar@ujcv.edu.hn>
2026-03-19 02:35:25 -0600

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,25 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "#007AFF" }}>
+    <Tabs screenOptions={{ 
+      tabBarActiveTintColor: "#007AFF",
+      headerShown: true,
+      headerTitle: "MA Deportivo" 
+    }}>
+      <Tabs.Screen
+        name="padel"
+        options={{
+          title: "Padel",
+          tabBarIcon: ({ color }) => <FontAwesome size={24} name="star" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="tennis"
+        options={{
+          title: "Tennis",
+          tabBarIcon: ({ color }) => <FontAwesome size={24} name="circle-thin" color={color} />,
+        }}
+      />
       <Tabs.Screen
         name="futbol"
         options={{
@@ -12,10 +30,17 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <FontAwesome size={24} name="soccer-ball-o" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="basket"
+        options={{
+          title: "Basket",
+          tabBarIcon: ({ color }) => <FontAwesome size={24} name="dribbble" color={color} />,
+        }}
+      />
       <Tabs.Screen name="index" options={{ href: null }} />
-      <Tabs.Screen name="basket" options={{ href: null }} />
-      <Tabs.Screen name="padel" options={{ href: null }} />
-      <Tabs.Screen name="tennis" options={{ href: null }} />
+      <Tabs.Screen name="f1" options={{ href: null }} />
+      <Tabs.Screen name="noticias" options={{ href: null }} />
+      <Tabs.Screen name="carrito" options={{ href: null }} />
     </Tabs>
   );
 }
