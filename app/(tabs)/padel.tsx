ae2d2db2f6920c3bbb5417870998cacc5009a4c9
Mauricio aguilar <mauricio.aguilar@ujcv.edu.hn>
2026-03-19 02:53:18 -0600

    Update padel.tsx

--- app/(tabs)/padel.tsx
+++ app/(tabs)/padel.tsx
@@ -1,5 +1,13 @@
 import { View, Text } from "react-native";
+import { View, Text } from "react-native";
 
+export default function Padel() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Padel</Text>
+    </View>
+  );
+}
 export default function Padel() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
