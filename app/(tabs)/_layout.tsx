7676dad5d6fa630ef6666cf134760d3491c2b897
Mauricio aguilar <mauricio.aguilar@ujcv.edu.hn>
2026-03-19 02:07:34 -0600

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,38 +6,25 @@ import { Image, StyleSheet, View } from "react-native";
 function LogoHeader() {
   return (
     <View style={styles.logoContainer}>
-      <Image
-        source={require("../../assets/images/logo.png")}
-        style={styles.logoImage}
-      />
+      <Image source={require("../../assets/images/logo.png")} style={styles.logoImage} />
     </View>
   );
 }
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "#007AFF",
-        tabBarLabelStyle: { fontSize: 10 },
-        headerTitle: () => <LogoHeader />,
-        headerTitleAlign: "center",
-        headerStyle: { backgroundColor: "#fff", height: 110 },
-      }}
-    >
-      <Tabs.Screen name="padel" options={{ title: "Padel", tabBarIcon: ({ color }) => <FontAwesome size={22} name="star" color={color} /> }} />
-      <Tabs.Screen name="tennis" options={{ title: "Tennis", tabBarIcon: ({ color }) => <FontAwesome size={22} name="circle-thin" color={color} /> }} />
-      <Tabs.Screen name="futbol" options={{ title: "Fútbol", tabBarIcon: ({ color }) => <FontAwesome size={22} name="soccer-ball-o" color={color} /> }} />
-      <Tabs.Screen name="basket" options={{ title: "Basket", tabBarIcon: ({ color }) => <FontAwesome size={22} name="dribbble" color={color} /> }} />
-      <Tabs.Screen name="f1" options={{ title: "F1", tabBarIcon: ({ color }) => <FontAwesome size={22} name="car" color={color} /> }} />
-      <Tabs.Screen name="noticias" options={{ title: "Noticias", tabBarIcon: ({ color }) => <FontAwesome size={22} name="newspaper-o" color={color} /> }} />
-      <Tabs.Screen name="carrito" options={{ title: "Carrito", tabBarIcon: ({ color }) => <FontAwesome size={22} name="shopping-cart" color={color} /> }} />
+    <Tabs screenOptions={{ tabBarActiveTintColor: "#007AFF", headerTitle: () => <LogoHeader />, headerTitleAlign: "center", headerStyle: { height: 110 } }}>
+      <Tabs.Screen name="padel" options={{ title: "Padel", tabBarIcon: ({ color }) => <FontAwesome size={24} name="star" color={color} /> }} />
+      <Tabs.Screen name="tennis" options={{ title: "Tennis", tabBarIcon: ({ color }) => <FontAwesome size={24} name="circle-thin" color={color} /> }} />
+      <Tabs.Screen name="futbol" options={{ title: "Fútbol", tabBarIcon: ({ color }) => <FontAwesome size={24} name="soccer-ball-o" color={color} /> }} />
+      <Tabs.Screen name="basket" options={{ title: "Basket", tabBarIcon: ({ color }) => <FontAwesome size={24} name="dribbble" color={color} /> }} />
+      <Tabs.Screen name="noticias" options={{ title: "Noticias", tabBarIcon: ({ color }) => <FontAwesome size={24} name="newspaper-o" color={color} /> }} />
       <Tabs.Screen name="index" options={{ href: null }} />
     </Tabs>
   );
 }
 
 const styles = StyleSheet.create({
-  logoContainer: { paddingTop: 10, justifyContent: "center", alignItems: "center" },
+  logoContainer: { justifyContent: "center", alignItems: "center" },
   logoImage: { width: 120, height: 60, resizeMode: "contain" },
 });
