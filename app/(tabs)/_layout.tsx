f78aa184a962fdec255cffddcf4189dd965337b3
Mauricio aguilar <mauricio.aguilar@ujcv.edu.hn>
2026-03-19 03:09:46 -0600

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,7 +7,7 @@ export default function TabLayout() {
     <Tabs screenOptions={{ 
       tabBarActiveTintColor: "#007AFF",
       headerShown: true,
-      headerTitle: "MA Deportivo"
+      headerTitle: "MA Deportivo" 
     }}>
       <Tabs.Screen
         name="futbol"
